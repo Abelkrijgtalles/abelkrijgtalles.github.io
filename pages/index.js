fd4a720a1fe4b28889ba9f1d0b4a837dabb8eea0
Abel van Hulst <abelvanhulst@gmail.com>
2022-02-09 18:17:23 +0100

    added titles

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
+import Head from "next/head";
+
 const Home = () => {
   return (
+    <>
+    <Head>
+      <title>Home | Abel krijgt alles</title>
+    </Head>
     <h1>Er is werk aan de winkel</h1>
+    </>
   );
 }
  
