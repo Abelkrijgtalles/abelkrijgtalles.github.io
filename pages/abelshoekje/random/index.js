9abdf980edfe3d8f348d5037cc4f9f53a984cebe
Abel van Hulst <abelvanhulst@gmail.com>
2022-02-10 15:28:51 +0100

    now?

---
+++ pages/abelshoekje/random/index.js
@@ -0,0 +1,7 @@
+const Random = () => {
+    return (
+        <h1>Dit is er</h1>
+    );
+}
+ 
+export default Random;
---
+++ pages/abelshoekje/random/mensen/index.js
@@ -0,0 +1,7 @@
+const Mensen = () => {
+    return (
+        <h1>Dit is er nog niet</h1>
+    );
+}
+ 
+export default Mensen;
