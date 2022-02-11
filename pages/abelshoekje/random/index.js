c617ec066482e2755ebc50c88b4c1e82954ce59b
Abel van Hulst <abelvanhulst@gmail.com>
2022-02-11 15:47:29 +0100

    fixed random ui

--- pages/abelshoekje/random/index.js
+++ pages/abelshoekje/random/index.js
@@ -11,7 +11,7 @@ const Random = () => {
             <div>
                 <h1>Random dingen</h1>
                 <Link href={'/abelshoekje/random/mensen'}><a className={styles.single}><h3>Mensen</h3></a></Link>
-                <Link href={'/abelshoekje/random/mensen'}><a className={styles.single}><h3>Posts (Latijn)</h3></a></Link>
+                <Link href={'/abelshoekje/random/posts'}><a className={styles.single}><h3>Posts (Latijn)</h3></a></Link>
             </div>
         </>
     );
