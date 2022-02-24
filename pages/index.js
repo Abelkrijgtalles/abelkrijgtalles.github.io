77451fa86ac6b3f16e1f82c0da19f56236144960
Abel van Hulst <abelvanhulst@gmail.com>
2022-02-24 12:04:27 +0100

    made some process to the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Image from "next/image";
+import Link from "next/link";
 
 const Home = () => {
   return (
@@ -10,6 +10,8 @@ const Home = () => {
       </Head>
       <div className="content">
         <h1 className={styles.welkom}>Welkom op de Abel krijgt alles website</h1>
+        <h2 className={styles.medium}>Ik ben Abel, je kent me vast wel. Misschien niet, maar dat maakt niet uit. Dit is mijn website.</h2>
+        <h2 className={styles.medium}>Hier kan je van alles doen: <Link href="/videos"><a>De nieuwste video&apos;s bekijken</a></Link>, <Link href="/blog"><a>mijn blog lezen</a></Link>, <Link href="/abelshoekje"><a>dingen die ik zelf heb gemaakt bekijken</a></Link> en nog veel meer!</h2>
       </div>
     </>
   );
