bc3ed18095dab81fde55e3d53ebb221a950a6841
Abel van Hulst <abelvanhulst@gmail.com>
2022-02-25 21:45:38 +0100

    now

--- pages/videos.js
+++ pages/videos.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../../styles/Mensen.module.css'
+import styles from '../styles/Mensen.module.css'
 
 export const getStaticProps = async () => {
 
