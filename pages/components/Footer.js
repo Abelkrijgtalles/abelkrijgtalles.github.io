bdb2a9054b467ade2b87b89a71763781dc6d26a1
Abel van Hulst <abelvanhulst@gmail.com>
2022-02-26 22:18:52 +0100

    added the source code in the footer

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 const Footer = () => {
     let date = new Date().getFullYear();
     return (
         <footer>
-            <p>Copyright Abel krijgt alles { date }. Met liefde gemaakt door Abel van Hulst</p>
+            <p>Copyright Abel krijgt alles { date }. Met liefde gemaakt door Abel van Hulst. Sourch code: <Link><a>abelr.tk/akaw</a></Link>.</p>
         </footer>
     );
 }
