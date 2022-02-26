a4f27ede1bfee590e204deda319319cb59fd8f20
Abel van Hulst <abelvanhulst@gmail.com>
2022-02-26 22:14:50 +0100

    blog update

--- pages/blog.js
+++ pages/blog.js
@@ -6,10 +6,15 @@ const Blog = () => {
     return (
         <div className={styles.row}>
             <div className='leftcolumn'>
-            <div className={styles.card}>
+                <div className={styles.card}>
+                    <h2><Link href={"https://abelkrijgtalles.nl"}><a>abelkrijgtalles.nl werkt nu!</a></Link></h2>
+                    <h5>26 Februari 2022</h5>
+                    <p>Als je nu naar <Link href="https://abelkrijgtalles.nl"><a>abelkrijgtalles.nl</a></Link> gaat, werkt het!</p>
+                </div>
+                <div className={styles.card}>
                     <h2><Link href={"https://abelkrijgtalles.nl"}><a>abelkrijgtalles.nl</a></Link></h2>
                     <h5>26 Februari 2022</h5>
-                    <p>Ik ben nu bezig <Link href={"https://abelkrijgtalles.nl"}><a>abelkrijgtalles.nl</a></Link> klaar te maken zodat ik het onder andere de url van de website kan maken.<br/>Deze link wil dan nog steeds werken, maar <Link href={"https://abelkrijgtalles.nl"}><a>abelkrijgtalles.nl</a></Link> werkt dan ook.</p>
+                    <p>Ik ben nu bezig <Link href={"https://abelkrijgtalles.nl"}><a>abelkrijgtalles.nl</a></Link> klaar te maken zodat ik het onder andere de url van de website kan maken.<br />Deze link wil dan nog steeds werken, maar <Link href={"https://abelkrijgtalles.nl"}><a>abelkrijgtalles.nl</a></Link> werkt dan ook.</p>
                 </div>
                 <div className={styles.card}>
                     <h2>Dit is het logo</h2>
