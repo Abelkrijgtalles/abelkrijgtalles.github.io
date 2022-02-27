235632c766564bd41240d9639041beb3fb0e7028
Abel van Hulst <abelvanhulst@gmail.com>
2022-02-27 12:08:03 +0100

    added email in footer

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     let date = new Date().getFullYear();
     return (
         <footer>
-            <p>Copyright Abel krijgt alles { date }. Met liefde gemaakt door Abel van Hulst. Source code: <Link href="https://abelr.tk/akaw"><a>abelr.tk/akaw</a></Link>.</p>
+            <p>Copyright Abel krijgt alles { date }. Met liefde gemaakt door Abel van Hulst. Source code: <Link href="https://abelr.tk/akaw"><a>abelr.tk/akaw</a></Link>. Mail naar <Link href="mailto:info@abelkrijgtalles.nl"><a>info@abelkrijgtalles.nl</a></Link></p>
         </footer>
     );
 }
