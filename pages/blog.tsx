18965fe163db6adfe438dcb22aa8de59c63b19aa
Abel van Hulst <abelvanhulst@gmail.com>
2022-03-06 21:11:04 +0100

    blog update

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -7,6 +7,17 @@ const Blog = () => {
         <div className={styles.row}>
             <div className='leftcolumn'>
                 <div className={styles.card}>
+                    <h2><Link href="https://abelkrijgtalles.nl"><a>abelkrijgtalles.nl</a></Link> is nu beter/meer ondersteunend voor oude webbrowser!</h2>
+                    <h5>6 Maart 2022</h5>
+                    <p>Ik ga nu misschien een beetje &quot;nerdie&quot; praten, maar als je daar oké mee bent laten we verder gaan. Door dat ik &quot;TypeScript&quot; gebruik (TypeScript is een soort van JavaScript++ van Microsoft) kan ik zeggen, hé, zorg dat de code werkt op elke JavaScript versie vanaf 1999.</p>
+                </div>
+                <div className={styles.card}>
+                    <h2><Link href={"https://abelkrijgtalles.nl"}><a>abelkrijgtalles.nl</a></Link> is nu ook te vinden op Google!</h2>
+                    <h5>6 Maart 2022</h5>
+                    <div className={styles.fakeimg}><Image src='/AbelkrijgtallesGoogle.png' width="384" height="128"></Image></div>
+                    <p>Als je Abel krijgt alles zoekt op Google (Hier boven aangegeven) dan bij het 4e zoek resultaat zie je gewoon dit!</p>
+                </div>
+                <div className={styles.card}>
                     <h2><Link href={"https://abelkrijgtalles.nl"}><a>abelkrijgtalles.nl</a></Link> werkt nu!</h2>
                     <h5>26 Februari 2022</h5>
                     <p>Als je nu naar <Link href="https://abelkrijgtalles.nl"><a>abelkrijgtalles.nl</a></Link> gaat, werkt het!</p>
