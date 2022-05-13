d3f1b32448cded2d76462bc4743cd319306036db
Abel <56084910+Abelkrijgtalles@users.noreply.github.com>
2022-05-13 21:04:06 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,8 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
     return (
         <Html>
-            <Head><script
-                async
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3986699835905370"
-            ></script></Head>
             <body>
                 <Main />
                 <NextScript />
